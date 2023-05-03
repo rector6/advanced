c562f48c769acec4b5b35dbfd028b156120c9cd0
rector6 <peteroche32@gmail.com>
2023-05-03 11:23:03 -0700

    update

--- src/components/AsK.vue
+++ src/components/AsK.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class=""></div>
+   <div class="queston" id="question">
+    <div class="container">
+      <h1 class="text-center q-head pt-4 pb-3">Frequently Asked Questions</h1>
+      <div class="card-subbody">
+        <p class="text-center pb-3">
+          Lorem ipsum dolor sit amet consetertur sadipcing tyorue<br />
+          nonnamy airmad tempor invidunt ut labour magna aliqyuamim<br />Lorem
+          nonnamy airmad tempor invidunt ut labour magna aliqyuamim<br />
+          Lorem ipsum diam.
+        </p>
+        <p class="text-center pb-4">
+          Have any questions,Our time is always readily available ! Get In Touch
+          With Us
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
